ae4e9f7d345ff6282e5ba9206c287f344d67484a
Dron <dron@dron-VirtualBox.(none)>
2013-10-19 12:07:56 -0400

    landing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '10825f8dab674b679f19b2c5830cabd7f45052d17ba49eaba0956ab05b54222b7fa93c5a6f019e26b6b8e049a65ac742029db1fa58ba44e8fb6032cbdeb7324d'
